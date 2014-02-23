a8ace39f671fb16a070a6a79b82e47ec967a3c0d
JamesMoore <jamesmooredeveloper@gmail.com>
2014-02-23 14:25:58 +0000

    download saves into directory structure

--- GoogleMusicManagerAPI/DownloadProcess.cs
+++ GoogleMusicManagerAPI/DownloadProcess.cs
@@ -52,14 +52,34 @@ namespace GoogleMusicManagerAPI
 
                 var bytes = await api.DownloadTrack(info.url);
 
-                var filename = track.track_number + " " + track.title + ".mp3";
+                var folderPath = GetOutputDirectory(track);
+                Directory.CreateDirectory(folderPath);
 
-                Path.GetInvalidFileNameChars().ToList().ForEach(p => filename = filename.Replace(p, '_'));
+                var filename = GetOutputFilename(track);
+                
+                var fullpath = Path.Combine(folderPath, filename);
 
-                File.WriteAllBytes(filename, bytes);
+                File.WriteAllBytes(fullpath, bytes);
             }
 
             return true;
+        }
+
+        private static string GetOutputFilename(DownloadTrackInfo track)
+        {
+            var filename = track.track_number + " " + track.title + ".mp3";
+            Path.GetInvalidFileNameChars().ToList().ForEach(p => filename = filename.Replace(p.ToString(), ""));
+            return filename;
+        }
+
+        private static string GetOutputDirectory(DownloadTrackInfo track)
+        {
+            var firstDirectoryLevel = track.artist;
+            Path.GetInvalidPathChars().ToList().ForEach(p => firstDirectoryLevel = firstDirectoryLevel.Replace(p.ToString(), ""));
+            var secondDirectoryLevel = track.album;
+            Path.GetInvalidPathChars().ToList().ForEach(p => secondDirectoryLevel = secondDirectoryLevel.Replace(p.ToString(), ""));
+            var folderPath = Path.Combine(firstDirectoryLevel, secondDirectoryLevel);
+            return folderPath;
         }
 
         public async Task<IEnumerable<DownloadTrackInfo>> GetTracks()
